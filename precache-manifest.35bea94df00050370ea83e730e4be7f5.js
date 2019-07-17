b4f5c7690e1b199e761c758e3fe2fd52cd90d2c9
Sergii Kasian <kasyansv1@gmail.com>
2019-07-17 16:18:38 +0300

    Updates

---
+++ precache-manifest.35bea94df00050370ea83e730e4be7f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7c79de6521e15ee213043de0453f4f8",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "2bd0644e65976d71e723",
+    "url": "/react_add-todo-form/static/css/main.b31bc05b.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_add-todo-form/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "2bd0644e65976d71e723",
+    "url": "/react_add-todo-form/static/js/main.3bcaa9db.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
